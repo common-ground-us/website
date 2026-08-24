3c4ad97da7a0990f64f255e1ae054e44d6a6277a
Keith Lietzke <keith.lietzke@gmail.com>
2026-08-24 12:24:00 -0400

    Add private /WorkingBoard mobile bookmark page (noindex, unlisted)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,7 @@ export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: { userAgent: "*", allow: "/", disallow: "/WorkingBoard/" },
     sitemap: "https://www.common-ground.us/sitemap.xml",
   };
 }
